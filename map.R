f6c357334d2e25a31bb596d5a28c44a130f71c0b
Kara Woo <kara.woo@sagebase.org>
2019-07-10 08:53:17 -0700

    Updated 2019 map

--- map.R
+++ map.R
@@ -33,16 +33,17 @@ gg1 <- ggplot(global) +
   ) +
   coord_map() +
   scale_fill_manual(values = c("grey90", "#5a498f")) +
-  scale_alpha_manual(values = c(1, .2)) +
+  scale_alpha_manual(values = c(1, .3)) +
   geom_point(data = coors, aes(long, lat), colour = "#e567a1", size = 1) +
   geom_text_repel(
     data = coors,
     aes(long, lat, label = location),
-    size = 3.5
+    size = 3.5,
+    force = 1.2
   ) +
   theme_void() +
   theme(legend.position = "none")
 
 ## gg1
 
-ggsave(gg1, filename = "amp-ad_updated_map_20180925_20.svg", width = 10, height = 8)
+ggsave(gg1, filename = "amp-ad_updated_map_20190710.svg", width = 10, height = 8)
